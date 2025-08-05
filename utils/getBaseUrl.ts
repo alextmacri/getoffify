013e5c402aeb0e4b62cf133ff05c5006d3f70dd6
alextmacri <alexmacri04@gmail.com>
2025-08-05 01:18:30 -0400

    updated get base url util

--- utils/getBaseUrl.ts
+++ utils/getBaseUrl.ts
@@ -1,7 +1,8 @@
 export default function getBaseUrl(): string {
     if (typeof window !== "undefined") {
         const host = window.location.host.replace("localhost", "127.0.0.1");
-        return `${window.location.protocol}//${host}`;
+        const baseUrl = `${window.location.protocol}//${host}${window.location.pathname}`;
+        return baseUrl.endsWith("/") ? baseUrl.slice(0, -1) : baseUrl;
     }
     return "";
 }
