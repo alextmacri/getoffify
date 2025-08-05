90ab1abbef4b682a737d4a0ec96d4887ae9ac8a6
alextmacri <alexmacri04@gmail.com>
2025-08-05 01:09:16 -0400

    updated layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
+import { Suspense } from "react";
+import { Spinner } from "@heroui/spinner";
 
 export const metadata: Metadata = {
   title: {
@@ -45,7 +47,11 @@ export default function RootLayout({
           <div className="relative flex flex-col h-screen">
             <Navbar />
             <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              {children}
+              <section className="flex flex-col items-center justify-center gap-12">
+                <Suspense fallback={<Spinner color="current" />}>
+                  {children}
+                </Suspense>
+              </section>
             </main>
             <footer className="w-full flex items-center justify-center py-3">
               <Link
