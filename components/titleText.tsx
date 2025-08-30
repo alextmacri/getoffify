b6aafc02f49a5bf76551f16054844a8aa814e042
alextmacri <alexmacri04@gmail.com>
2025-08-30 18:29:57 -0400

    updated title text

--- components/titleText.tsx
+++ components/titleText.tsx
@@ -5,9 +5,9 @@ import { title } from "./primitives";
 export default function TitleText() {
   return (
     <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Get your music from&nbsp;</span>
+        <span className={title()}>Get your library from&nbsp;</span>
         <span className={title({ color: "green" })}>Spotify&nbsp;</span>
-        <span className={title()}>and take them&nbsp;</span>
+        <span className={title()}>and take it&nbsp;</span>
         <span className={title({ color: "pink" })}>back.&nbsp;</span>
     </div>
   );
